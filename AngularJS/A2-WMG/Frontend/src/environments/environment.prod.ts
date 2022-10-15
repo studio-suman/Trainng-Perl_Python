eecf526bc8b30c3d9bb25dc8d6385bce581d0580
Suman Saha <studio.suman@gmail.com>
2022-10-16 00:24:27 +0530

    Configuring Firebase

--- AngularJS/A2-WMG/Frontend/src/environments/environment.prod.ts
+++ AngularJS/A2-WMG/Frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
     authDomain: "a2wmg-69ed7.firebaseapp.com",
     projectId: "a2wmg-69ed7",
     storageBucket: "a2wmg-69ed7.appspot.com",
-    messagingSenderId: "1052836547541",
+    //messagingSenderId: "1052836547541",
     appId: "1:1052836547541:web:bd36e1702a5e1d0942cb5c"
   }
 };
