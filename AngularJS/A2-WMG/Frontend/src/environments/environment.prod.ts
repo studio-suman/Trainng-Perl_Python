ef7ee4993b3d69593b00b02a6ffa610dc6567d48
Suman Saha <studio.suman@gmail.com>
2022-10-16 00:23:32 +0530

    Configuring Firebase

--- AngularJS/A2-WMG/Frontend/src/environments/environment.prod.ts
+++ AngularJS/A2-WMG/Frontend/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBsld0WLuiYJycLUPtF8tZq6sjCW1jnNiQ",
+    authDomain: "a2wmg-69ed7.firebaseapp.com",
+    projectId: "a2wmg-69ed7",
+    storageBucket: "a2wmg-69ed7.appspot.com",
+    messagingSenderId: "1052836547541",
+    appId: "1:1052836547541:web:bd36e1702a5e1d0942cb5c"
+  }
 };
--- AngularJS/A2-WMG/Frontend/src/environments/environment.ts
+++ AngularJS/A2-WMG/Frontend/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBsld0WLuiYJycLUPtF8tZq6sjCW1jnNiQ",
+    authDomain: "a2wmg-69ed7.firebaseapp.com",
+    projectId: "a2wmg-69ed7",
+    storageBucket: "a2wmg-69ed7.appspot.com",
+    messagingSenderId: "1052836547541",
+    appId: "1:1052836547541:web:bd36e1702a5e1d0942cb5c"
+  }
 };
 
 /*
