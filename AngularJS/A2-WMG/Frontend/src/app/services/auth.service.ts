e4f92cd8f2a84f3f912d3ecf933e556245ce5170
Suman Saha <studio.suman@gmail.com>
2022-10-17 20:36:19 +0530

    Updated Services

--- AngularJS/A2-WMG/Frontend/src/app/services/auth.service.ts
+++ AngularJS/A2-WMG/Frontend/src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth'
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import IUser from 'src/app/models/user.models';
 
 @Injectable({
@@ -8,23 +8,38 @@ import IUser from 'src/app/models/user.models';
 })
 export class AuthService {
 
+  private userCollection: AngularFirestoreCollection<IUser>
+
   constructor(
     private auth: AngularFireAuth,
     private db: AngularFirestore
-  ) { }
+  ) { 
+    this.userCollection = db.collection("users")
+  }
 
   public async createUser(userData: IUser) {
+
+    if(!userData.password) {
+      throw new Error("Password Not Provided!");
+    }
 
     const userCred = await this.auth.createUserWithEmailAndPassword(
       userData.email as string, userData.password as string
     )
-    await this.db.collection('users').add({
+
+    if(!userCred.user) {
+      throw new Error("User Can't be Found");
+    }  
+
+    await this.userCollection.doc(userCred.user.uid).set({
         name: userData.name,
         email: userData.email,
         age: userData.age,
         phoneNumber: userData.phoneNumber
       }
     )
-
+      await userCred.user.updateProfile({
+        displayName: userData.name,
+      })
   }
 }
